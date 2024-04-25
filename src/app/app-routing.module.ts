8dba42f6c287afee2b57f087a339b0bcf5c29701
DNC202 <nguyencongdanh2002@gmail.com>
2024-04-25 10:40:41 +0000

    create detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FoodDetailsComponent } from './food-details/food-details.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'search/:searchItem', component:HomeComponent}
+  {path:'search/:searchItem', component:HomeComponent},
+  {path:'food/:id', component:FoodDetailsComponent}
 ];
 
 @NgModule({
